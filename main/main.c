7b6b0c9c8ffc0ec1116dd733f1e8598a14a04d53
armin langhofer <armin@langhofer.at>
2020-09-13 20:05:13 +0200

    cleanups

--- main/main.c
+++ main/main.c
@@ -24,21 +24,17 @@
 
 #define PIN_KNX_RX                      17
 #define PIN_KNX_TX                      16
-
 #define KNX_HALF_BIT_TIME_MS            15
-#define KNX_HIGH_TIME_US                35 // 104us / 3
 
-#define ESP_INTR_FLAG_DEFAULT           0
+#define ESP_INTR_FLAG_DEFAULT 0
 
-int toggle=0;
-
-static xQueueHandle gpio_evt_queue = NULL;
+static xQueueHandle queue_knxrx = NULL;
 static xQueueHandle queue_knxtx = NULL;
 
 static intr_handle_t s_timer_handle_knxrx;
 static intr_handle_t s_timer_handle_knxtx;
 
-#define MAX_OCTETS 1000 // TODO: buid ringbuffer
+#define MAX_OCTETS 1000
 int octets[MAX_OCTETS];
 int current_octet=0;
 
@@ -46,14 +42,20 @@ int bits_read=0;
 int startbit_level=-1;
 int stopbit_level=-1;
 int paritybit=-1;
+int bits_send=0;
+int tmp_knx_tx=0;
+int sendcount=0;
+int tmp_sendbyte=0;
+int waitcount=0;
+int octets_printed = 0;
+int index_octets_processed = 0;
+int timercount=0;
+
 
 unsigned char calc_even_parity_bit(unsigned char x){
     unsigned int count = 0, i, b = 1;
-
-    for(i = 0; i < 8; i++){
+    for(i = 0; i < 8; i++)
         if( x & (b << i) ){count++;}
-    }
-
     return (count % 2);
 }
 
@@ -64,26 +66,18 @@ unsigned char calc_odd_horizontal_parity_byte(int len, unsigned char* data) {
     return (~tmp & 0xFF);
 }
 
-
-int bits_send=0;
-int tmp_knx_tx=0;
-
-const unsigned char controlbyte = 0xBC;
-
 #define SENDBUFFER_LEN 8
-unsigned char sendbuffer[SENDBUFFER_LEN] = {0xBC // knx control byte,
+unsigned char sendbuffer[100] = {
+    0xBC, // knx control byte
     0x12, 0x03, 0x58, 0x03, 0xE1, 0x00, 0x81, 
-    0x69 // checksum, calc_odd_horizontal_parity_byte()
+    0x69 // knx checksum calc_odd_horizontal_parity_byte()
     };
 
-int tmp_sendbyte=0;
-int waitcount=0;
-
-static void timer_isr_knx_send_bit(void* arg)
+static void isr_knx_tx_timer(void* arg)
 {
     portBASE_TYPE xHigherPriorityTaskWoken;
 
-    // restart timer
+    // re-enable timer
     TIMERG1.int_clr_timers.t1 = 1;
     TIMERG1.hw_timer[1].config.alarm_en = 1;
 
@@ -103,6 +97,11 @@ static void timer_isr_knx_send_bit(void* arg)
         // wait x2 bytes
         bits_send=-30;
         tmp_sendbyte = 0;
+        sendcount++;
+    }
+
+    if (sendcount == 2) {
+        return;
     }
 
     char octet = sendbuffer[tmp_sendbyte];
@@ -130,19 +129,30 @@ static void timer_isr_knx_send_bit(void* arg)
         tmp_sendbyte++;
     } 
 
+    if (false)
+    {
+        timer_config_t config;
+        timer_get_config(TIMER_GROUP_1, TIMER_1, &config);
+        if (config.alarm_en)
+            timer_pause(TIMER_GROUP_1, TIMER_1);
+    }
+
     bits_send++;
 
     return 0;
 }
 
 
-static void timer_isr_knx_receive_bit(void* arg)
+static void isr_knx_rx_timer(void* arg)
 {
     portBASE_TYPE xHigherPriorityTaskWoken;
 
     // restart timer
     TIMERG0.int_clr_timers.t0 = 1;
     TIMERG0.hw_timer[0].config.alarm_en = 1;
+
+    // for debugging w/ oscilloscope
+    //gpio_set_level(PIN_LED_2_DEBUG_OSCILLOSCOPE, ++toggle % 2);
 
     // starbit
     if (bits_read==0) { 
@@ -158,36 +168,39 @@ static void timer_isr_knx_receive_bit(void* arg)
 
         // read knx bit
         int bit = gpio_get_level(PIN_KNX_RX);
-        bit = bit == 1 ? 0 : 1; // knx-bits are inverted
+        bit = bit == 1 ? 0 : 1; // invert. knx sends 1 as logic 0 and vice-versa, see knx spec
 
-        // knx octets (8 bits): lsb first, msb last.
+        // first bit read is LSB. last bit is MSB. we read octets (8 bits)
+        // knx databits: lsb first, msb last.
         octets[current_octet] = octets[current_octet] >> 1;
         octets[current_octet] |=  bit<<7;
     }
 
     if (bits_read==9) {
         paritybit = gpio_get_level(PIN_KNX_RX);
-        paritybit = paritybit == 1 ? 0 : 1; // knx-bits are inverted
+        paritybit = paritybit == 1 ? 0 : 1; // invert. knx sends 1 as logic 0 and vice-versa, see knx spec
     }
 
+
     if (bits_read==10) {
-        // done reading bits
+        // all bits read.
 
         stopbit_level = gpio_get_level(PIN_KNX_RX);
-
+        
         // pause timer and wait for new startbit. this triggers a new interrupt that will reenable the timer
+
         timer_config_t config;
         timer_get_config(TIMER_GROUP_0, TIMER_0, &config);
         if (config.alarm_en)
             timer_pause(TIMER_GROUP_0, TIMER_0);
 
-        // re-enable interrupts for next startbit
+        // reenable interrupts for next startbit
         gpio_intr_enable(PIN_KNX_RX);
 
         // prepare next octet
         bits_read=-1;
 
-        // knx has a 30ms high-level. move timer half bit-time foreward and try to read in "the middle"
+        // stknx signals 30ms high-level. move timer half bit-time foreward and try to read in "the middle"
         timer_group_set_alarm_value_in_isr(TIMER_GROUP_0, TIMER_0, KNX_HALF_BIT_TIME_MS);
 
         //dummy, killme.
@@ -195,64 +208,62 @@ static void timer_isr_knx_receive_bit(void* arg)
 
         current_octet++;
 
-        xQueueSendToFrontFromISR( gpio_evt_queue, &byKeyDown, &xHigherPriorityTaskWoken );
+        // debug read octet to serial console
+        xQueueSendToFrontFromISR( queue_knxrx, &byKeyDown, &xHigherPriorityTaskWoken );
     }
 
     bits_read++;
 }
 
-void init_tg1_t1()
+void init_knx_tx_timer()
 {
     timer_config_t config = {
-            .alarm_en = TIMER_ALARM_EN,
-            .counter_en = TIMER_PAUSE,          // do not start timer
-            .intr_type = TIMER_INTR_LEVEL,
-            .counter_dir = TIMER_COUNT_UP,
-            .auto_reload = TIMER_AUTORELOAD_EN,
-            .divider = 80   				    // 80 MHz (12.5nS) APB_CLK clock. e.g. 80 = 1uS per timer tick
+            .alarm_en = TIMER_ALARM_EN,			//Alarm Enable
+            .counter_en = TIMER_PAUSE,			//If the counter is enabled it will start incrementing / decrementing immediately after calling timer_init()
+            .intr_type = TIMER_INTR_LEVEL,	    //Is interrupt is triggered on timer’s alarm (timer_intr_mode_t)
+            .counter_dir = TIMER_COUNT_UP,	    //Does counter increment or decrement (timer_count_dir_t)
+            .auto_reload = TIMER_AUTORELOAD_EN,	//If counter should auto_reload a specific initial value on the timer’s alarm, or continue incrementing or decrementing.
+            .divider = 80   				    //Divisor of the incoming 80 MHz (12.5nS) APB_CLK clock. E.g. 80 = 1uS per timer tick
     };
     timer_init(TIMER_GROUP_1, TIMER_1, &config);
     timer_set_counter_value(TIMER_GROUP_1, TIMER_1, 0);
 
-    timer_isr_register(TIMER_GROUP_1, TIMER_1, &timer_isr_knx_send_bit, NULL, ESP_INTR_FLAG_LEVEL3, &s_timer_handle_knxtx);
-    timer_set_alarm_value(TIMER_GROUP_1, TIMER_1, KNX_HIGH_TIME_US);
+    timer_isr_register(TIMER_GROUP_1, TIMER_1, &isr_knx_tx_timer, NULL, ESP_INTR_FLAG_LEVEL3, &s_timer_handle_knxtx);
+    timer_set_alarm_value(TIMER_GROUP_1, TIMER_1, 35); //104/3us
 
     timer_enable_intr(TIMER_GROUP_1, TIMER_1);
 }
 
-void init_tg0_t0()
+void init_knx_rx_timer()
 {
     timer_config_t config = {
-            .alarm_en = TIMER_ALARM_EN,
-            .counter_en = TIMER_PAUSE,          // do not start timer
-            .intr_type = TIMER_INTR_LEVEL,
-            .counter_dir = TIMER_COUNT_UP,
-            .auto_reload = TIMER_AUTORELOAD_EN,
-            .divider = 80   				    // 80 MHz (12.5nS) APB_CLK clock. e.g. 80 = 1uS per timer tick
+            .alarm_en = TIMER_ALARM_EN,			//Alarm Enable
+            .counter_en = TIMER_PAUSE,			//If the counter is enabled it will start incrementing / decrementing immediately after calling timer_init()
+            .intr_type = TIMER_INTR_LEVEL,	    //Is interrupt is triggered on timer’s alarm (timer_intr_mode_t)
+            .counter_dir = TIMER_COUNT_UP,	    //Does counter increment or decrement (timer_count_dir_t)
+            .auto_reload = TIMER_AUTORELOAD_EN,	//If counter should auto_reload a specific initial value on the timer’s alarm, or continue incrementing or decrementing.
+            .divider = 80   				    //Divisor of the incoming 80 MHz (12.5nS) APB_CLK clock. E.g. 80 = 1uS per timer tick
     };
     timer_init(TIMER_GROUP_0, TIMER_0, &config);
     timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
-    timer_isr_register(TIMER_GROUP_0, TIMER_0, &timer_isr_knx_receive_bit, NULL, ESP_INTR_FLAG_LEVEL3, &s_timer_handle_knxrx);
+    timer_isr_register(TIMER_GROUP_0, TIMER_0, &isr_knx_rx_timer, NULL, ESP_INTR_FLAG_LEVEL3, &s_timer_handle_knxrx);
+
     timer_enable_intr(TIMER_GROUP_0, TIMER_0);
 }
 
-// isr that is fired on each rising edge on knx read
+// interrupt that is triggered on each rising edge on knx read
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     // assuming starbit
 
-    // disable interrupt, start timer. timer will re-enable this interrupt.
+    // process octet, this isr will be re-enabled within timer isr
     gpio_intr_disable(PIN_KNX_RX);
 
-    // timer's isr will fire in about 15ms
+    // wait about 15us, will be reconfigured to 104us within timer isr
     timer_start(TIMER_GROUP_0, TIMER_0);
 }
 
-
-int octets_printed = 0;
-int index_octets_processed = 0;
-
-static void task_debug_console(void* arg)
+static void task_knxrx(void* arg)
 {
     for(;;) {
 
@@ -262,17 +273,15 @@ static void task_debug_console(void* arg)
         int index_start_frame=-1;
         int index_end_frame=-1;
 
-        if( pdTRUE == xQueueReceive( gpio_evt_queue, &byDummy, portMAX_DELAY) ) 
+        if( pdTRUE == xQueueReceive( queue_knxrx, &byDummy, portMAX_DELAY) ) 
         {
-            // print received bytes for debugging
+            // print received bytes.
             for (; octets_printed < current_octet; octets_printed++) {
                 printf("%02x ", octets[octets_printed]);
             }
             
             uint8_t tmp=0;
             int horizontal_parity=-1;
-
-            // try finding index_end_frame by calcing parity byte
             for ( int i=index_octets_processed; i<MAX_OCTETS; i++ ) {
 
                 // start of KNX TP1 Frame
@@ -292,9 +301,6 @@ static void task_debug_console(void* arg)
             }
 
             if (index_start_frame>0 && index_end_frame>0) {
-
-                // found valid frame
-
                 index_octets_processed = index_end_frame;
 
                 struct KNX_TP1_Frame *frame = malloc(sizeof *frame);
@@ -321,13 +327,13 @@ static void task_debug_console(void* arg)
                     gpio_set_level(PIN_LED_4_CLONE_LIGHT, octets[index_start_frame + 7] & 0x1);
                 }
 
-                free(frame); // remove me to process frame
+                free(frame);
             }
         }
     }
 }
 
-static void task_debug_console_knxtx(void* arg)
+static void task_knxtx(void* arg)
 {
     for(;;) {
         // not needed. killme.
@@ -335,19 +341,19 @@ static void task_debug_console_knxtx(void* arg)
 
         if( pdTRUE == xQueueReceive( queue_knxtx, &byDummy, portMAX_DELAY) ) 
         {
-            //printf("task_debug_console_knxtx...\n");
+            //printf("task_knxtx...\n");
         }
     }
 }
 
-static void task_sendknx(void* arg)
+static void task_knx_send(void* arg)
 {
 
-    init_tg1_t1();
+    init_knx_tx_timer();
     timer_start(TIMER_GROUP_1, TIMER_1);
 
     for (;;) {
-        //printf("task_sendknx...\n");
+
         vTaskDelay(10000 / portTICK_RATE_MS);
     }
 }
@@ -372,7 +378,7 @@ void setup_leds() {
     gpio_config(&io_conf);
 }
 
-void test_leds() {
+void blink_all_leds() {
     for (int i=1; i>=0; i--) {
         gpio_set_level(PIN_LED_0, i);
         vTaskDelay(75 / portTICK_RATE_MS);
@@ -394,11 +400,11 @@ void test_leds() {
 void setup_knx_rx_pin() {
     gpio_config_t io_conf;
 
-    // rising edge indicates knx startbit
     io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
     io_conf.pin_bit_mask = 1ULL << PIN_KNX_RX;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
+
     gpio_config(&io_conf);
 }
 
@@ -410,46 +416,51 @@ void setup_knx_tx_pin() {
     io_conf.pin_bit_mask = 1ULL << PIN_KNX_TX;
     io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
+
     gpio_config(&io_conf);
 }
 
+void setup_knx_reading() {
+    setup_knx_rx_pin();
+
+    queue_knxrx = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(task_knxrx, "task_knxrx", 2048, NULL, 10, NULL);
+
+    init_knx_rx_timer();
+
+    // rising edge indicates knx startbit
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(PIN_KNX_RX, gpio_isr_handler, (void*) PIN_KNX_RX);
+}
+
+void setup_knx_writing() {
+    setup_knx_tx_pin();
+
+    queue_knxtx = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(task_knxtx, "task_knxtx", 2048, NULL, 10, NULL);
+
+    xTaskCreate(task_knx_send, "task_knx_send", 2048, NULL, 10, NULL);
+}
 
 void app_main()
 {
     setup_leds();
+    blink_all_leds();
 
-    test_leds();
+    setup_knx_reading();
+    setup_knx_writing();
 
-    setup_knx_rx_pin();
-    setup_knx_tx_pin();
-
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    xTaskCreate(task_debug_console, "task_debug_console", 2048, NULL, 10, NULL);
-
-    queue_knxtx = xQueueCreate(10, sizeof(uint32_t));
-    xTaskCreate(task_debug_console_knxtx, "task_debug_console_knxtx", 2048, NULL, 10, NULL);
-
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(PIN_KNX_RX, gpio_isr_handler, (void*) PIN_KNX_RX);
-
-    // knx rx timer
-    init_tg0_t0();
-
-    // knx tx timer
-    xTaskCreate(task_sendknx, "task_sendknx", 2048, NULL, 10, NULL);
-    
     printf("waiting for KNX frames ...\n");
-    long uptime=0;
+    long uptime = 0;
     while(1) {
-        int delay=5000;
+        vTaskDelay(5000 / portTICK_RATE_MS);
         uptime++;
-        vTaskDelay(delay / portTICK_RATE_MS);
 
-        long sec = (uptime*delay/1000 / 1) % 60;
-        long min = (uptime*delay/1000 / 60 ) % 60;
-        long hour = (uptime*delay/1000 / 60 / 60 ) % 60;
-        long day = (uptime*delay/1000 / 60 / 60 / 24 ) % 24;
+        int sec = ( uptime * 5000 / 1000 / 1 ) % 60;
+        int min = ( uptime * 5000 / 1000 / 60 ) % 60;
+        int hour = ( uptime * 5000 / 1000 / 60 / 60 ) % 60;
+        int day = ( uptime * 5000 / 1000 / 60 / 60 / 24 ) % 24;
 
-        printf("heap_caps_get_free_size %d uptime: %lddays %ldhours %ldmin %lds ...\n", heap_caps_get_free_size(MALLOC_CAP_8BIT), day, hour, min, sec);
+        printf("heap_caps_get_free_size %dbytes uptime:%ddays %dh %d' %d\" ...\n", heap_caps_get_free_size(MALLOC_CAP_8BIT), day, hour, min, sec);
     }
 }
