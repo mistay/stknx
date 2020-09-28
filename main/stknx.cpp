965800b88d3c6051e3d24e3c39eb38fa275090de
mrxa13 <f.auernigg@commend.com>
2020-09-28 02:45:34 +0200

    add watchdog to tx thread again

--- main/stknx.cpp
+++ main/stknx.cpp
@@ -312,8 +312,8 @@ static void task_knxrx(void* arg)
         int index_end_frame=-1;
         esp_task_wdt_reset();
 
-        //if (pdTRUE == xQueueReceive(queue_knxrx, &byDummy, 10000 / portTICK_RATE_MS))
-        if (pdTRUE == xQueueReceive(queue_knxrx, &byDummy, portMAX_DELAY))
+        if (pdTRUE == xQueueReceive(queue_knxrx, &byDummy, 10000 / portTICK_RATE_MS))
+        //if (pdTRUE == xQueueReceive(queue_knxrx, &byDummy, portMAX_DELAY))
         {
             // print received bytes.
             //for (; octets_printed < current_octet; octets_printed++) {
@@ -471,6 +471,7 @@ void setup_knx_reading(void (*knx_frame_received)(KnxTelegram &telegram))
     queue_knxrx = xQueueCreate(10, sizeof(uint32_t));
 
     xTaskCreatePinnedToCore(task_knxrx, "task_knxrx", STKNX_LOOP_STACK, NULL, 10, &knxRxHandle, STKNX_CORE);
+    esp_task_wdt_add(knxRxHandle);
 
     init_knx_rx_timer();
 
@@ -488,7 +489,7 @@ void setup_knx_writing()
     //xTaskCreate(task_knx_send, "task_knx_send", STKNX_LOOP_STACK, NULL, 10, NULL);
 
     xTaskCreatePinnedToCore(task_knx_send, "task_knx_send", STKNX_LOOP_STACK, NULL, 10, &knxTxHandle, STKNX_CORE);
-    //esp_task_wdt_add(knxTxHandle);
+    esp_task_wdt_add(knxTxHandle);
 }
 
 
